fa90e6a1e0e2bf9e1ae2e81162db937d01b104e9
belartale <belartale@gmail.com>
2021-06-20 23:15:12 +0300

    save

--- TS/6_guards.ts
+++ TS/6_guards.ts
@@ -4,4 +4,21 @@ function strip(x: string | number) {
   if (typeof x === "number") {
     return x.toFixed(2);
   }
+  return x.trim();
+}
+
+class MyResponse {
+  header = "response header",
+  message = "response message"
+}
+
+class MyError {
+  header = "error header",
+  message = "error message"
+}
+
+function handle(res:MyResponse | MyError) {
+  if (res instanceof MyResponse) {
+    
+  }
 }
