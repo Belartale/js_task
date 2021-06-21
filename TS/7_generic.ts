fa12ea190c725ff8871e93ed7dae6a5722bc711b
belartale <belartale@gmail.com>
2021-06-21 12:27:31 +0300

    add new coments

--- TS/7_generic.ts
+++ TS/7_generic.ts
@@ -1,3 +1,5 @@
+// шаблонизаторы который позволяет создавать классы с динамическими типами
+
 const arrayOfNumber: Array<number> = [1, 1, 2, 3, 5];
 const arrayOfString: Array<string> = ["hello", "artur"];
 
